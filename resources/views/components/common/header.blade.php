13aaf5042aefbc11ea9f1c57f942a2a1f7b288b5
munablamu <coffee.git@gmail.com>
2023-12-01 09:40:04 +0900

    grayをslateに変更

--- resources/views/components/common/header.blade.php
+++ resources/views/components/common/header.blade.php
@@ -1,15 +1,15 @@
-<header class="text-gray-300 bg-gray-800 body-font fixed top-0 w-full z-10">
+<header class="text-slate-300 bg-slate-800 body-font fixed top-0 w-full z-10">
   <div class="container mx-auto flex flex-wrap px-5 py-3 flex-row items-center justify-between">
     <!-- logo -->
-    <a href="{{ route('top') }}" class="flex title-font font-medium items-center text-gray-100">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+    <a href="{{ route('top') }}" class="flex title-font font-medium items-center text-slate-100">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-slate-100 p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
       <span class="ml-3 text-xl">Classic Music Review App</span>
     </a>
     <!-- primary nav -->
     <nav class="hidden md:ml-auto md:mr-auto md:flex md:flex-wrap items-center text-base justify-center">
-      <a href="#" class="mr-5 hover:text-gray-100">このサイトの使い方</a>
+      <a href="#" class="mr-5 hover:text-slate-100">このサイトの使い方</a>
     </nav>
     <!-- secondary nav -->
     <div class="hidden md:flex items-center space-x-1">
@@ -28,7 +28,7 @@
     </div>
     <!-- hamburger button -->
     <div id="hamburger" class="md:hidden">
-      <button type="button" class="block text-gray-400 hover:text-gray-300 focus:text-gray-200 focus:outline-none">
+      <button type="button" class="block text-slate-400 hover:text-slate-300 focus:text-slate-200 focus:outline-none">
         <svg class="h-8 w-8 fill-current" viewBox="0 0 24 24">
           <path id="open" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
           <path id="close" class="hidden" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
@@ -38,16 +38,16 @@
   </div>
   <div id="hamburger_nav" class="hidden md:hidden text-right mx-auto px-5">
     <nav class="px-2 pt-2 pb-4 flex flex-col p-0">
-      <a href="#" class="block px-2 py-1 rounded-md hover:text-gray-100 hover:bg-gray-600 focus:bg-gray-500">このサイトの使い方</a>
+      <a href="#" class="block px-2 py-1 rounded-md hover:text-slate-100 hover:bg-slate-600 focus:bg-slate-500">このサイトの使い方</a>
       @guest
-        <a href="{{ route('login') }}" class="block px-2 py-1 rounded-md hover:text-gray-100 hover:bg-gray-600 focus:bg-gray-500">Login</a>
-        <a href="{{ route('register') }}" class="block px-2 py-1 rounded-md hover:text-gray-100 hover:bg-gray-600 focus:bg-gray-500">Signup</a>
+        <a href="{{ route('login') }}" class="block px-2 py-1 rounded-md hover:text-slate-100 hover:bg-slate-600 focus:bg-slate-500">Login</a>
+        <a href="{{ route('register') }}" class="block px-2 py-1 rounded-md hover:text-slate-100 hover:bg-slate-600 focus:bg-slate-500">Signup</a>
       @endguest
       @auth
-        <a href="{{ route('home') }}" class="block px-2 py-1 rounded-md hover:text-gray-100 hover:bg-gray-600 focus:bg-gray-500">Home</a>
+        <a href="{{ route('home') }}" class="block px-2 py-1 rounded-md hover:text-slate-100 hover:bg-slate-600 focus:bg-slate-500">Home</a>
         <form action="{{ route('logout') }}" method="post">
           @csrf
-          <button type="submit" class="w-full text-right block px-2 py-1 rounded-md hover:text-gray-100 hover:bg-gray-600 focus:bg-gray-500">Logout</a>
+          <button type="submit" class="w-full text-right block px-2 py-1 rounded-md hover:text-slate-100 hover:bg-slate-600 focus:bg-slate-500">Logout</a>
         </form>
       @endauth
     </nav>
