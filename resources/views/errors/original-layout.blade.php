8363ee3c79e4b5d8468fc571e66e3e697b7edbec
munablamu <coffee.git@gmail.com>
2023-12-12 17:29:22 +0900

    errorページのページタイトルを追加（暫定的）

--- resources/views/errors/original-layout.blade.php
+++ resources/views/errors/original-layout.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-layout :title="'エラーページ'">
     <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div class="flex flex-col items-center justify-center">
             <h1 class="text-7xl font-bold font-Ubuntu text-slate-400 dark:text-slate-500">@yield('code')</h1>
