b6c157fe8937e2f65451d8b0178c2a5a3e78a8ed
munablamu <coffee.git@gmail.com>
2023-12-20 09:26:15 +0900

    見た目の軽微な修正

--- resources/views/components/common/help.blade.php
+++ resources/views/components/common/help.blade.php
@@ -1,4 +1,6 @@
 <div id="help" class="fixed bottom-28 sm:bottom-16 left-4 max-w-xl rounded-3xl p-4 mr-4 shadow-2xl bg-slate-600 text-slate-100 dark:bg-slate-100 dark:text-slate-700 z-50">
-  <div id="help-close" class="text-xl cursor-pointer text-right"><i class="fa-solid fa-circle-xmark"></i></div>
-  <p>{!! $message !!}</p>
+  <div class="flex flex-row">
+    <p>{!! $message !!}</p>
+    <div id="help-close" class="text-xl cursor-pointer ml-2"><i class="fa-solid fa-circle-xmark"></i></div>
+  </div>
 </div>
