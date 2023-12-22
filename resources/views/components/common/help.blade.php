ba9ea3263d612d38f632ad6cf02408dbab29b950
munablamu <coffee.git@gmail.com>
2023-12-22 17:59:58 +0900

    helpの色を変更

--- resources/views/components/common/help.blade.php
+++ resources/views/components/common/help.blade.php
@@ -1,4 +1,4 @@
-<div id="help" class="fixed bottom-20 sm:bottom-16 left-4 max-w-xl rounded-3xl p-4 mr-4 shadow-2xl bg-slate-600 text-slate-100 dark:bg-slate-100 dark:text-slate-700 z-20">
+<div id="help" class="fixed bottom-20 sm:bottom-16 left-4 max-w-xl rounded-3xl p-4 mr-4 shadow-2xl bg-slate-900 text-slate-250 dark:bg-slate-125 dark:text-slate-800 z-20">
   <div class="flex flex-row">
     <p>{!! $message !!}</p>
     <div id="help-close" class="text-xl cursor-pointer ml-2"><i class="fa-solid fa-circle-xmark"></i></div>
