1a05a1f42edca9398451f959b1078dcdeaaec3bc
munablamu <coffee.git@gmail.com>
2023-12-25 15:47:59 +0900

    修正

--- config/mail.php
+++ config/mail.php
@@ -102,6 +102,7 @@ return [
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
+        'transport' => env('MAIL_FROM_TRANSPORT', 'smtp'),
     ],
 
     /*
